cae3709c13854d917288b6c96309a3cc0bc480fb
as-uh <aloksinghmintorian@hotmail.com>
2020-11-13 19:31:14 +0000

    13Nov

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,36 +1,45 @@
-from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect, Http404
 from django.urls import reverse
-
-from .models import Topic
+from django.contrib.auth.decorators import login_required
+from .models import Topic, Entry
 from .forms import TopicForm
 
 # Create your views here.
 def index(request):
     return render(request,'learning_logs/index.html')
 
+@login_required
 def topics(request):
-    topics=Topic.objects.order_by('date_added')
+    topics=Topic.objects.filter(owner=request.user).order_by('date_added')
     context={'topic':topics}
     return render(request,'learning_logs/topics.html',context)
 
+@login_required
 def topic(request, topic_id):
     topic=Topic.objects.get(id=topic_id)
+    if topic.owner != request.user:
+        raise Http404
     entries=topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
 
+@login_required
 def new_topic(request):
     if request.method != 'POST':
         form = TopicForm()
     else:
         form= TopicForm(request.POST)
         if form.is_valid():
-            form.save()
+            new_topic = form.save(commit=False)
+            new_topic.owner = request.user
+            new_topic.save()
             return redirect('learning_logs:topics')
     context={'form': form}
     return render(request,'learning_logs/new_topic.html', context)
 
+
+@login_required
 def new_entry(request,topic_id):
     # Add a new entery for a perticular job
     topic=Topic.objects.get(id=topic_id)
@@ -46,9 +55,14 @@ def new_entry(request,topic_id):
     context={'topic':topic,'form':form}
     return render(request, 'learning_logs/new_entry.html',context)
 
+
+@login_required
 def edit_entry(request, entry_id):
     entry =Entry.objects.get(id=entry_id)
     topic=entry.topic
+    if topic.owner != request.user:
+        raise Http404
+    
     if request.method !='POST':
         form=entryForm(instance=entry)
     else:
